07700e0aede5918816e5e322d8e3e6dd1c36c07f
nurnoby admin <nurnoby323@gmail.com>
2025-09-12 20:45:20 +0600

    project setup

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
-import express from 'express';
-const app = express();
+import { Server } from 'http';
+import app from './app';
 
 const port = 8000;
 
-app.listen(port, () => console.log("App is listening on port",port))
+async function main() {
+    const server: Server = app.listen(port, () => {console.log("Server is running on port", port)})
+}
+main()
