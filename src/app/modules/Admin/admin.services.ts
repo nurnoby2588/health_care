f920f8272aedc74692bf079cc92a41c100d654c1
nurnoby admin <nurnoby323@gmail.com>
2025-09-13 14:58:12 +0600

    remove code duplicancy

--- src/app/modules/Admin/admin.services.ts
+++ src/app/modules/Admin/admin.services.ts
@@ -2,30 +2,39 @@ import { Prisma, PrismaClient } from "../../../generated/prisma"
 const prisma = new PrismaClient()
 const getAllFromDB = async (params: any) => {
     // type sefty is give []
-    const andCondition :Prisma.AdminWhereInput[] = []
+    const andCondition: Prisma.AdminWhereInput[] = []
+    const adminSearchableFielda = ['name', 'email']
+
+    // [
+    //             {
+    //                 name: {
+    //                     contains: params.searchTerm,
+    //                     //for case insensitive
+    //                     mode: 'insensitive'
+    //                 }
+    //             },
+    //             {
+    //                 email: {
+    //                     contains: params.searchTerm,
+    //                     mode: 'insensitive'
+    //                 }
+    //             },
+    //         ]
+
     if (params.searchTerm) {
         andCondition.push({
-            OR: [
-                {
-                    name: {
-                        contains: params.searchTerm,
-                        //for case insensitive
-                        mode: 'insensitive'
-                    }
-                },
-                {
-                    email: {
-                        contains: params.searchTerm,
-                        mode: 'insensitive'
-                    }
-                },
-            ]
+            OR: adminSearchableFielda.map(field => ({
+                [field]: {
+                    contains: params.searchTerm,
+                    mode: 'insensitive'
+                }
+            }))
         })
     }
     // console.dir(andCondition, { depth: 'infinity' })
     // where conditon want {} object type . but andCondition this array give []. that way {AND: andCondition}
     // whereConditon : Prisma.AdminWhereInput prisma type checking
-    const whereConditon : Prisma.AdminWhereInput = {AND: andCondition}
+    const whereConditon: Prisma.AdminWhereInput = { AND: andCondition }
     const result = await prisma.admin.findMany({
         where: whereConditon
     });
