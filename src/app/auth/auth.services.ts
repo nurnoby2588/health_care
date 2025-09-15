173edb73125ebdde7c7352c9d9c659a701699e9f
nurnoby admin <nurnoby323@gmail.com>
2025-09-15 12:52:20 +0600

    resolve type error in refresh token and add active filter

--- src/app/auth/auth.services.ts
+++ src/app/auth/auth.services.ts
@@ -1,11 +1,14 @@
+import { email } from "zod";
 import { jwtHelper } from "../../healper/jwtHelper";
 import prisma from "../../Shared/prisma";
 import * as bcrypt from 'bcrypt'
+import { UserStatus } from "../../generated/prisma";
 
 const loginUser = async (payload: { email: string, password: string }) => {
     const userData = await prisma.user.findUniqueOrThrow({
         where: {
-            email: payload.email
+            email: payload.email,
+            status: UserStatus.ACTIVE
         }
     })
 
@@ -23,8 +26,22 @@ const loginUser = async (payload: { email: string, password: string }) => {
     return { accessToken, refreshToken, needPasswordChange: userData.needPasswordChange }
 }
 
-const refreshToken = async(refreshToken: string)=>{
-    console.log({refreshToken})
+const refreshToken = async (refreshToken: string) => {
+    const decodeToken = jwtHelper.verifyToken(refreshToken, "refreshSecret") 
+    const userData = await prisma.user.findUniqueOrThrow({
+        where: {
+            id: decodeToken.id,
+            status: UserStatus.ACTIVE
+        }
+    })
+    const data = {
+        id: userData.id,
+        email: userData.email,
+        role: userData.role
+    }
+    const accessToken = jwtHelper.generateToken(data, "abc", '1h')
+    return { accessToken, needPasswordChange: userData.needPasswordChange }
+
 }
 
 export const AuthServices = { loginUser, refreshToken }
--- src/healper/jwtHelper.ts
+++ src/healper/jwtHelper.ts
@@ -1,6 +1,13 @@
-import jwt from 'jsonwebtoken'
-const generateToken = (payload: any, secret: string, expiresIn: string | number) => {
-    const token = jwt.sign(payload, secret, {algorithm:"HS256", expiresIn } as  jwt.SignOptions);
+import jwt, { JwtPayload, Secret } from 'jsonwebtoken'
+const generateToken = (payload: any, secret: Secret, expiresIn: string | number) => {
+    const token = jwt.sign(payload, secret, { algorithm: "HS256", expiresIn } as jwt.SignOptions);
     return token;
 };
-export const jwtHelper ={generateToken}
+const verifyToken = (token: string, secret: Secret) => {
+    try {
+         return jwt.verify(token, secret) as JwtPayload 
+    } catch (error) {
+        throw new Error("Yor are not authorized!")
+    }
+}
+export const jwtHelper = { generateToken, verifyToken }
